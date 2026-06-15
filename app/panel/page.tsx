5a848a6e3de81aebdd047adca071ee1b3d755a2d
Gaël Tournier <gael.tournier32@gmail.com>
2026-06-15 08:49:11 +0200

    fix(Style fix)

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -42,17 +42,17 @@ export default function Page() {
             <p className={'font-title font-bold '}>Gaël Tournier - Espace Client</p>
           </div>
 
-          <div className={'bg-white ml-3 w-[95%] md:w-fit p-6 shadow-big border-3  md:ml-6'}>
-            <h1 className={'text-6xl font-black font-title'}>Bienvenue sur votre espace!</h1>
+          <div className={'bg-white ml-1 w-[95%] md:w-fit p-6 shadow-big border-3  md:ml-6'}>
+            <h1 className={'text-4xl md:text-6xl font-black font-title'}>Bienvenue sur votre espace!</h1>
             <p className={'text-xl mt-6 '}>Cet espace permet de centraliser les ressources, et de faciliter la communication!</p>
           </div>
 
 
           <TimeLine Updates={updates}/>
 
-            <div className={'flex flex-col w-full md:flex-row md:w-[97%] gap-6 items-stretch justify-center mt-6 md:mx-6 ml-3 '}>
+            <div className={'flex flex-col w-full md:flex-row md:w-[95%] gap-6 items-stretch justify-center mt-6 md:mx-6 ml-1'}>
 
-                <div className={'bg-white p-6 w-95 md:w-[50%] border-3 shadow-small'}>
+                <div className={'bg-white p-6 w-[95%] md:w-[50%] border-3 shadow-small'}>
                   <h2 className={'text-2xl font-bold border-l-3 border-main pl-3 font-title'}>Messages</h2>
                   <div>
                     <p>Aucun message à afficher</p>
@@ -60,7 +60,7 @@ export default function Page() {
                 </div>
 
 
-                <div className={'w-95 md:w-[48%] bg-white p-6 border-3 shadow-small '}>
+                <div className={'w-[95%] md:w-[48%] bg-white p-6 border-3 shadow-small '}>
                     <h2 className={'text-2xl font-bold border-l-3 border-main pl-3 font-title'}>Mes Documents</h2>
 
                     <div className={'flex flex-col gap-2 mt-3'}>
